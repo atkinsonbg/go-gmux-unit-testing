eeb4641805c9cc5d013644eae5f12a89ada51e99
Brandon Atkinson <tehremo@gmail.com>
2020-12-01 22:02:09 -0500

    Update commands.sh

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "Cloning the repo"
-git clone https://github.com/atkinsonbg/go-gmux-unit-testing.git .
+echo "Setting up the Postgres user"
+POSTGRES="psql --username ${POSTGRES_USER}"
 
-# echo "Setting up the Postgres user"
-# POSTGRES="psql --username ${POSTGRES_USER}"
+echo "Creating database: ${DBNAME}"
+$POSTGRES <<EOSQL
+CREATE DATABASE "${DBNAME}" OWNER ${POSTGRESUSER};
+EOSQL
 
-# echo "Creating database: ${DBNAME}"
-# $POSTGRES <<EOSQL
-# CREATE DATABASE "${DBNAME}" OWNER ${POSTGRESUSER};
-# EOSQL
+echo "Creating schema..."
+psql -d ${DBNAME} -a -U${POSTGRESUSER} -f ./scripts/init.sql
 
-# echo "Creating schema..."
-# psql -d ${DBNAME} -a -U${POSTGRESUSER} -f ./scripts/init.sql
-
-# echo "Running Go Tests"
-# go test -v ./... &
+echo "Running Go Tests"
+go test -v ./... &
